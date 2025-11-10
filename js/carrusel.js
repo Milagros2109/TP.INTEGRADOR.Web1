b18e68dc6f652fec61c5b46435edc85b9c488524
urbanijose <urbanijosemaria6@gmail.com>
2025-11-10 18:38:25 -0300

    Cambios en el carrusel

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,33 +1,18 @@
-const img1 = document.getElementById("img1");
-const img2 = document.getElementById("img2");
-const img3 = document.getElementById("img3");
-
-function izquierda() {
-    if (window.getComputedStyle(img1).display !== "none") {
-        img1.style.display = "none";
-        img3.style.display = "block";
-    } else if (window.getComputedStyle(img2).display !== "none") {
-        img2.style.display = "none";
-        img1.style.display = "block";
-    } else if (window.getComputedStyle(img3).display !== "none") {
-        img3.style.display = "none";
-        img2.style.display = "block";
+let imagenes = ["./img/productos/reloj1.png", "./img/productos/auris1.png", "./img/productos/Celu4.png"];
+let i = 0;
+console.log(imagenes);
+function anterior () {
+    i--;
+    if (i == -1) {
+        i = imagenes.length - 1;
     }
+    document.getElementById("img1").src = imagenes[i];
 }
 
-function derecha() {
-    if (window.getComputedStyle(img1).display !== "none") {
-        img1.style.display = "none";
-        img2.style.display = "block";
-    } else if (window.getComputedStyle(img2).display !== "none") {
-        img2.style.display = "none";
-        img3.style.display = "block";
-    } else if (window.getComputedStyle(img3).display !== "none") {
-        img3.style.display = "none";
-        img1.style.display = "block";
+function siguiente () {
+    i++;
+    if (i == imagenes.length) {
+        i = 0;
     }
+    document.getElementById("img1").src = imagenes[i];
 }
-
-const prueba = document.getElementsByClassName("content");
-console.log(prueba);
-
